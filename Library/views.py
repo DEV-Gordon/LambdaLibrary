5c367d700bf7c5c95880fa18a4c90e42a9ec681e
Carlos Zarate <128831952+DEV-Gordon@users.noreply.github.com>
2025-06-09 13:09:43 -0500

    optimizations

--- Library/views.py
+++ Library/views.py
@@ -30,6 +30,7 @@ class CategoryView(ListView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
         context['category'] = self.category
         return context
     
